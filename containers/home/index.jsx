6441637b7ed6e7899d8a61efe94ae49dac344480
rmkofficial <kmert0@outlook.com>
2024-08-27 11:22:23 +0300

    Added the OurServicesComponent to the HomeContainer

--- containers/home/index.jsx
+++ containers/home/index.jsx
@@ -16,6 +16,7 @@ const HomeContainer = () => {
       <MostPopularVacanciesSection />
       <FeelTheBenefitComponent />
       <PopularClinicsComponent />
+      <OurServicesComponent />
     </div>
   );
 };
