66c62883c9a312a8df8e193bac95aff706561a08
Fırat Kıl <72046564+firatkill@users.noreply.github.com>
2024-08-28 01:25:49 +0300

    featurecards icon colors

--- components/mostPopularVacanciesSection/featureCard/index.jsx
+++ components/mostPopularVacanciesSection/featureCard/index.jsx
@@ -4,13 +4,9 @@ export default function FeatureCard(props) {
   const data = props.data;
   return (
     <div className="group hover:shadow-xl transition-all duration-200 cursor-pointer rounded-lg flex justify-start  items-center p-2 ">
-      <div className="w-20   h-20 rounded-md  group-hover:shadow-none  transition-all duration-200 shadow-xl flex items-center justify-center group-hover:bg-primary ">
+      <div className=" w-20 p-4  h-20 rounded-md  group-hover:shadow-none  transition-all duration-200 shadow-xl flex items-center justify-center group-hover:bg-primary ">
         <Image
-          className="group-hover:bg-red-500  "
-          style={{
-            filter:
-              " invert(100%) sepia(0%) saturate(7473%) hue-rotate(110deg) brightness(105%) contrast(109%)",
-          }}
+          className="filter group-hover:brightness-0 group-hover:invert"
           src={`/assets/icons/${data.img}`}
           alt=""
           width={72}
