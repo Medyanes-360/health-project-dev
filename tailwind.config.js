b6fa3fa7113d41a257253c630887bb05a68ffbc0
Fırat Kıl <72046564+firatkill@users.noreply.github.com>
2024-08-28 20:23:37 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,30 @@ module.exports = {
         poppins: ["var(--font-poppins)", "sans-serif"],
         inter: ["var(--font-inter)", "sans-serif"],
       },
-
+      screens: {
+        xs: "0px",
+      },
       colors: {
-        primary: "#52B8AB",
+        primary: {
+          DEFAULT: "#52B8AB",
+          dark: "#0B8071",
+          light: "rgba(175, 255, 245, 0.3)",
+        },
+        black: {
+          DEFAULT: "#000000",
+          light: "#18191C",
+          dark: "#1E1E1E",
+        },
+        white: {
+          DEFAULT: "#FFFFFF",
+          // gri renk:
+          dark: "#F1F2F4",
+          // disabled field'lar için:
+          // disabled:""
+        },
+        secondary: "#52B8AB",
         bgDarkWhite: "#F1F2F4",
+        bgDarkText: "#5E6670",
         secondary: "#111827",
         third: "#7D7987",
         fourth: "#FFFFFF",
