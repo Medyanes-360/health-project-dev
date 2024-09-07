3fb2c92a473af8da2bf415639e021719114413d5
Fırat Kıl <72046564+firatkill@users.noreply.github.com>
2024-09-07 13:48:38 +0300

    comment

--- app/(homeLayout)/treatments/[treatment]/page.jsx
+++ app/(homeLayout)/treatments/[treatment]/page.jsx
@@ -1,5 +1,12 @@
 import TreatmentPageContainer from "@/containers/treatmentPage";
 
+//burada sayfanın propları alınarak container'a gönderilecek. container'dan tüm componentlara ilgili data gönderilecek.
+// sayfa tüm treatment'lar için aynı. yalnızca treatmentların bilgileri değişiyor.
+// Örneğin /hair-transplant sayfasında hair transplant hakkında bilgiler varken ,
+// /dental-treatments sayfasında dental treatment ile alakalı bilgiler gösterilecek, fakat yapı aynı olacak.
+// bu bilgileri getProps ile çekeceğiz. daha sonra container'a props olarak göndereceğiz. Container'dan geri kalan tüm sectionlara dağıtılacak.
+
+// WARN : getStaticProps mu getServersideProps mu? şimdilik mock data oluşturarak kullanacağız
 export default function treatmentPage() {
   return <TreatmentPageContainer />;
 }
