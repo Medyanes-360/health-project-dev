fefcbfa02ed76b2f93c97ddfd9ca5452dbefef50
Fırat Kıl <72046564+firatkill@users.noreply.github.com>
2024-09-19 21:59:33 +0300

    Update index.jsx

--- components/clinic/patientReviewsSection/patientReviewsList/index.jsx
+++ components/clinic/patientReviewsSection/patientReviewsList/index.jsx
@@ -5,7 +5,7 @@ export default function PatientReviewsList({ reviews }) {
   return (
     <div className="flex-1 flex flex-col items-center ">
       {reviews.map((review, index) => {
-        return <PatientReviewsListItem review={review} />;
+        return <PatientReviewsListItem key={index} review={review} />;
       })}
       <ButtonComponent className="!py-[15px] !px-[60px] mt-3 w-[228px] !rounded-[34px] bg-primary">
         <p className="pr-2.5 text-white">See More </p>
