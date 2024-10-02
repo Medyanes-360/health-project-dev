c82b494cc8135bfa392dc7a243b354e297f6093b
ahmad <itxti909@gmail.com>
2024-10-02 14:45:29 +0300

    expand

--- globalElements/faqs/expand/index.jsx
+++ globalElements/faqs/expand/index.jsx
@@ -1,6 +1,4 @@
 "use client";
-import { MotionDiv } from "@/globalElements/motion";
-import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
@@ -12,7 +10,7 @@ export default function ExpandingQuestions({ title, description, index }) {
   };
 
   return (
-    <div className="my-1 flex justify-between gap-5 w-full">
+    <div className="my-1 flex justify-between gap-5 w-full ">
       <div className="flex-1">
         {/* Title Button */}
         <button
@@ -21,11 +19,7 @@ export default function ExpandingQuestions({ title, description, index }) {
           }`}
           onClick={toggleExpanded}
         >
-          <div
-            className={`
-               w-full px-2 py-3 rounded-t-xl flex-1
-              `}
-          >
+          <div className="w-full px-2 py-3 rounded-t-xl flex-1">
             <h1
               className={`font-medium text-start w-full px-5 ${
                 isExpanded ? "text-white" : "text-secondary"
@@ -33,26 +27,38 @@ export default function ExpandingQuestions({ title, description, index }) {
             >
               {title}
             </h1>
-          </MotionDiv>
+          </div>
 
           <div
             className={`px-5 text-4xl font-light transition-all duration-200 ${
-              isExpanded ? " rotate-[45deg] text-white-dark" : "text-third"
+              isExpanded ? "rotate-[45deg] text-white-dark" : "text-third"
             }`}
           >
             +
           </div>
         </button>
 
-        {isExpanded && (
-          <div
-            className={` pb-3 animate-fadeIn rounded-b-xl transition-all duration-200 px-2 py-3
-              ${isExpanded ? "bg-primary/30" : ""}
-            `}
-          >
-            <p className="font-light px-5">{description}</p>
-          </div>
-        )}
+        {/* Animated Description */}
+        <AnimatePresence initial={false}>
+          {isExpanded && (
+            <motion.div
+              key="content"
+              initial={{ height: 0, opacity: 0 }}
+              animate={{ height: "auto", opacity: 1 }}
+              exit={{ height: 0, opacity: 0 }}
+              transition={{ duration: 0.4 }}
+              className="overflow-hidden"
+            >
+              <div
+                className={`pb-3 rounded-b-xl transition-all duration-200 px-2 py-3 ${
+                  isExpanded ? "bg-primary/30" : ""
+                }`}
+              >
+                <p className="font-light px-5">{description}</p>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
