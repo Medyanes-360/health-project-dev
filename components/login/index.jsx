74a55f790381a41cdb1bd7d05626c82dff65b114
Afra <afra_yavuz@hotmail.com>
2024-10-04 21:03:38 +0300

    login/formik&yup

--- components/login/index.jsx
+++ components/login/index.jsx
@@ -1,18 +1,27 @@
 "use client";
-import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function LoginComponent() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Burada login işlemlerini gerçekleştirin
-    console.log("Giriş denemesi:", email, password);
-    // İsteğe bağlı olarak formu sıfırlayabilirsiniz
-    // setEmail("");
-    // setPassword("");
-  };
+  // Formik yapılandırması
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string("Enter your email")
+        .email("Enter a valid email")
+        .required("Email is required"),
+      password: Yup.string("Enter your password")
+        .min(8, "Password should be of minimum 8 characters length")
+        .required("Password is required"),
+    }),
+    onSubmit: (values) => {
+      // Giriş işlemleri burada yapılacak
+      console.log("Attempt to enter:", values);
+    },
+  });
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
@@ -23,7 +32,10 @@ export default function LoginComponent() {
         <h1 className="text-lg sm:text-xl font-semibold text-start text-gray-700 mb-4 sm:mb-6">
           Sign in
         </h1>
-        <form onSubmit={handleSubmit} className="space-y-4 sm:space-y-6 w-full">
+        <form
+          onSubmit={formik.handleSubmit}
+          className="space-y-4 sm:space-y-6 w-full"
+        >
           <div>
             <label
               htmlFor="email"
@@ -34,14 +46,23 @@ export default function LoginComponent() {
             </label>
             <input
               id="email"
+              name="email"
               type="text"
               placeholder="Enter your email or phone number"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className="w-full px-3 py-2 border-[0.5px] bg-primary bg-opacity-10 border-primary text-sm rounded-lg sm:rounded-xl focus:outline-none focus:ring-1 focus:ring-teal-500"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className={`w-full px-3 py-2 border-[0.5px] bg-primary bg-opacity-10 border-primary text-sm rounded-lg sm:rounded-xl focus:outline-none focus:ring-1 focus:ring-teal-500 ${
+                formik.touched.email && formik.errors.email
+                  ? "border-red-500"
+                  : ""
+              }`}
             />
+            {formik.touched.email && formik.errors.email ? (
+              <p className="text-red-500 text-sm mt-1">{formik.errors.email}</p>
+            ) : null}
           </div>
+
           <div>
             <label
               htmlFor="password"
@@ -51,14 +72,25 @@ export default function LoginComponent() {
             </label>
             <input
               id="password"
+              name="password"
               type="password"
               placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              className="w-full px-3 py-2 border-[0.5px] text-sm bg-primary bg-opacity-10 border-primary rounded-lg sm:rounded-xl focus:outline-none focus:ring-1 focus:ring-teal-500"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className={`w-full px-3 py-2 border-[0.5px] bg-primary bg-opacity-10 border-primary text-sm rounded-lg sm:rounded-xl focus:outline-none focus:ring-1 focus:ring-teal-500 ${
+                formik.touched.password && formik.errors.password
+                  ? "border-red-500"
+                  : ""
+              }`}
             />
+            {formik.touched.password && formik.errors.password ? (
+              <p className="text-red-500 text-sm mt-1">
+                {formik.errors.password}
+              </p>
+            ) : null}
           </div>
+
           <button
             type="submit"
             className="w-full bg-primary text-white py-2 px-4 rounded-lg sm:rounded-xl focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50"
