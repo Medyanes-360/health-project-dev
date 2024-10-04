d4ace7edf0423237d519d771f5178edb8988a14f
Fırat Kıl <72046564+firatkill@users.noreply.github.com>
2024-10-04 17:13:21 +0300

    fix

--- components/layout/navigationHeader/headerbarComponent/headerbarSelectField/index.jsx
+++ components/layout/navigationHeader/headerbarComponent/headerbarSelectField/index.jsx
@@ -1,10 +1,9 @@
 "use client";
 import { diseaseDoctorHospitalMockData } from "@/data/mocks/searchInputMockData";
 import AutoCompleteInput from "@/globalElements/autoCompleteInput";
-import CitySelectDropdown from "@/globalElements/locationSelectDropdowns/citySelectDropdown";
 import CountrySelectDropdown from "@/globalElements/locationSelectDropdowns/countrySelectDropdown";
-import StateSelectDropdown from "@/globalElements/locationSelectDropdowns/stateSelectDropdown";
-import { City, Country, State } from "country-state-city";
+
+import { Country } from "country-state-city";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -14,9 +13,9 @@ const HeaderbarSelectField = ({ flags, countryNames, placeholder }) => {
   const data = diseaseDoctorHospitalMockData;
 
   const countries = Country.getAllCountries();
-  const [selectedCountry, setSelectedCountry] = useState(null);
-  const [selectedState, setSelectedState] = useState(null);
-  const [selectedCity, setSelectedCity] = useState(null);
+  const [selectedCountry, setSelectedCountry] = useState(
+    countries.find((country) => country.isoCode == defaultCountry)
+  );
   const [searchValue, setSearchValue] = useState("");
 
   const handleSubmit = (e) => {
@@ -30,8 +29,6 @@ const HeaderbarSelectField = ({ flags, countryNames, placeholder }) => {
     });
   };
 
-  const cities = City.getCitiesOfState("TR", "34");
-  const states = State.getStatesOfCountry("TR");
   return (
     // TODO: Grid olsun ve proplarla ayarlansın, Flag'lerin kalitesi, input width'i , dropdownlar focus gittiğinde otomatik kapanacak ,
     <form
@@ -47,16 +44,7 @@ const HeaderbarSelectField = ({ flags, countryNames, placeholder }) => {
         countryNames={countryNames}
         toLeft
       />
-      <StateSelectDropdown
-        setSelectedState={setSelectedState}
-        selectedState={selectedState}
-        states={states}
-      />{" "}
-      <CitySelectDropdown
-        setSelectedCity={setSelectedCity}
-        selectedCity={selectedCity}
-        cities={cities}
-      />
+
       <div className="flex w-full  border-l pl-2 ">
         <Image
           src="/assets/icons/searchIcon.svg"
