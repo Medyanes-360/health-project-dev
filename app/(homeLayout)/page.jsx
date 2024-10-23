88e107e099f17f49830f83c023cf8d85ecf3a8d8
Afra <afra_yavuz@hotmail.com>
2024-10-23 20:41:42 +0300

    assistant Chat

--- app/(homeLayout)/page.jsx
+++ app/(homeLayout)/page.jsx
@@ -1,5 +1,14 @@
+import AssistantChat from "@/components/assistant/assistantChat";
 import HomeContainer from "@/containers/home";
 
 export default function Home() {
-  return <HomeContainer />;
+  return (
+    <div>
+      <HomeContainer />
+      <div className="relative min-h-screen">
+        {/* Sağlık Turizmi Chat Asistanı */}
+        <AssistantChat />
+      </div>
+    </div>
+  );
 }
