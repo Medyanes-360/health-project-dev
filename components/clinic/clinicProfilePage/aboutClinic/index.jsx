4b4dc21bc53a032bea6ae7b4d7adbd1716998624
justsedaunal <justsedaunal@outlook.com>
2024-11-11 18:28:25 +0300

    revize

--- components/clinic/clinicProfilePage/aboutClinic/index.jsx
+++ components/clinic/clinicProfilePage/aboutClinic/index.jsx
@@ -10,24 +10,59 @@ export default function AboutClinic() {
     sub_heading_2: "About the clinic",
     text: "Implant4Life by Smilefactory, a private medical center in Budapest, Hungary, specializes in Dental Treatment and Maxillofacial Surgery. The clinic integrates advanced digital technologies and was awarded the All-on-4.",
     hrefText: "Read more",
-    sub_heading_3: "Highlights",
-    text_1: "2015 Year of foundation",
-    text_2: "3 doctors",
-    sub_heading_4: "Accepted payment method",
-    text_3: "Cash",
-    text_4: "3 doctors",
-    sub_text:
-      "No payment is needed now. You will be charged directly at the clinic when you arrive.",
-    hrefText_1: "View more information",
+    highlights: [
+      { title: "2015 Year of foundation", text: "Year of foundation" },
+      { title: "3 doctors", text: "Number of doctors" },
+    ],
+
+    sections: [
+      {
+        heading: "Implantology Expertise",
+        content:
+          "The expert team at Smilefactory specializes in implantology, offering permanent solutions for missing teeth. The clinic uses the latest techniques and high-quality implants to ensure new teeth look, feel, and function like natural ones. The implant services aim to improve oral health, boost confidence, and provide lasting results.",
+      },
+      {
+        heading: "Oral Surgery Services",
+        content:
+          "The medical team prioritizes patient safety and comfort, using cutting-edge technology and precise techniques for optimal outcomes.",
+        list: [
+          "Wisdom teeth extractions",
+          "Corrective jaw surgeries",
+          "Other complex dental surgeries",
+        ],
+      },
+      {
+        heading: "Comprehensive Dental Care",
+        content:
+          "In addition to implantology and oral surgery, Smilefactory offers a full range of dental services, including preventive care, restorative treatments, cosmetic dentistry, and orthodontics, covering all dental needs under one roof.",
+      },
+      {
+        heading: "Preventive and Cosmetic Services",
+        content:
+          "Preventive care services, such as regular check-ups and cleanings, maintain oral health, while cosmetic dentistry helps patients achieve their ideal smile.",
+      },
+      {
+        heading: "Patient Care",
+        content:
+          "Patient care is a top priority. The experienced and compassionate team ensures treatments are tailored to individual needs and strives to make visits as comfortable and stress-free as possible.",
+      },
+      {
+        heading: "Commitment to Excellence",
+        content:
+          "The clinic upholds the highest dental care standards, welcoming new and returning patients with exceptional service.",
+      },
+    ],
   };
+
   return (
     <>
+      <hr className="mt-8 mb-8"></hr>
       <section data-v-5b0e0b05="">
-        <h2 class="font-semibold text-xl mb-4">
+        <h2 className="font-poppins text-lg font-semibold leading-6 mb-4">
           Implant4Life by Smilefactory: more details about the clinic
         </h2>{" "}
         <div className="flex flex-col gap-4 sm:flex-row sm:justify-between sm:items-center">
-          <div className="flex items-center">
+          <div className="flex items-center mt-4">
             <Image
               src="/assets/clinic/location.svg"
               alt="Location Icon"
@@ -35,60 +70,42 @@ export default function AboutClinic() {
               height={24}
               className="mr-2"
             />
-            <p className="text-[#52B8AB] font-bold">{data.main_heading}</p>{" "}
+            <p className="text-[#52B8AB] font-poppins text-base font-semibold leading-6">
+              {data.main_heading}
+            </p>{" "}
           </div>
         </div>
-        <div class="py-5 space-y-4">
-          <p>
-            Implant4Life by Smilefactory, located in Budapest, Hungary, features
-            a state-of-the-art facility that combines advanced dental technology
-            with a warm, welcoming atmosphere, ensuring top-quality dental care.
+        <div className="pt-5 space-y-2">
+          <p className="font-poppins text-sm font-normal leading-6">
+            {data.text}
           </p>
-          <h3 class="text-xl ">Implantology Expertise</h3>
-          <p>
-            The expert team at Smilefactory specializes in implantology,
-            offering permanent solutions for missing teeth. The clinic uses the
-            latest techniques and high-quality implants to ensure new teeth
-            look, feel, and function like natural ones. The implant services aim
-            to improve oral health, boost confidence, and provide lasting
-            results.
-          </p>
-          <h3 class="text-xl ">Oral Surgery Services</h3>
-          <ul class="list-disc pl-5">
-            <li>Wisdom teeth extractions</li>
-            <li>Corrective jaw surgeries</li>
-            <li>Other complex dental surgeries</li>
-          </ul>
-          <p>
-            The medical team prioritizes patient safety and comfort, using
-            cutting-edge technology and precise techniques for optimal outcomes.
-          </p>
-          <h3 class="text-xl ">Comprehensive Dental Care</h3>
-          <p>
-            In addition to implantology and oral surgery, Smilefactory offers a
-            full range of dental services, including preventive care,
-            restorative treatments, cosmetic dentistry, and orthodontics,
-            covering all dental needs under one roof.
-          </p>
-          <h3 class="text-xl">Preventive and Cosmetic Services</h3>
-          <p>
-            Preventive care services, such as regular check-ups and cleanings,
-            maintain oral health, while cosmetic dentistry helps patients
-            achieve their ideal smile.
-          </p>
-          <h3 class="text-xl ">Patient Care</h3>
-          <p>
-            Patient care is a top priority. The experienced and compassionate
-            team ensures treatments are tailored to individual needs and strives
-            to make visits as comfortable and stress-free as possible.
-          </p>
-          <h3 class="text-xl ">Commitment to Excellence</h3>
-          <p>
-            The clinic upholds the highest dental care standards, welcoming new
-            and returning patients with exceptional service.
-          </p>
-          <h3 class="text-xl ">Additional Services</h3>
+
+          {data.sections.map((section, index) => (
+            <div key={index}>
+              <h3 className="font-poppins text-sm font-normal leading-6">
+                {section.heading}
+              </h3>
+              <p className="font-poppins text-sm font-normal leading-6">
+                {section.content}
+              </p>
+              {section.list && (
+                <ul className="list-disc pl-5">
+                  {section.list.map((item, listIndex) => (
+                    <li
+                      key={listIndex}
+                      className="font-poppins text-sm font-normal leading-6"
+                    >
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          ))}
         </div>
+        <h3 className="font-poppins text-sm font-normal leading-6 mb-8">
+          Additional Services
+        </h3>
         <AdditionalServicesDropdown />
       </section>
     </>
