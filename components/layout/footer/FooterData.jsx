e53e9c20168676e2c0c051162b3a07909624c294
Afra <afra_yavuz@hotmail.com>
2024-11-13 20:51:06 +0300

    footer fixes

--- components/layout/footer/FooterData.jsx
+++ components/layout/footer/FooterData.jsx
@@ -1,12 +1,13 @@
 import ExtraLargePageContainer from "@/containers/extraLargePageContainer";
 import ButtonComponent from "@/globalElements/Button";
+import Link from "next/link";
 
 const FooterData = () => {
   return (
     <div className="w-full pt-5">
-       <h1 className="text-center text-4xl text-white mb-10">
+      <h1 className="text-center text-4xl text-white mb-10">
         Medyanes<span className="text-primary">360</span>
-       </h1>
+      </h1>
       <div className="w-full flex flex-col justify-center lg:flex-row lg:justify-between lg:items-center gap-6 lg:gap-12 text-fourth">
         <div className="flex gap-7 flex-col lg:flex-row text-center lg:text-start">
           <div className="space-y-4 flex-1 ">
@@ -20,28 +21,44 @@ const FooterData = () => {
             </h1>
           </div>
 
-          <div className="space-y-3 flex-1 ">
+          <div className="space-y-3 flex flex-col ">
             <p className="mb-1">Partner </p>
-            <p className="font-thin">Became a partner</p>
-            <p className="font-thin">Partner login </p>
+            <Link href="#" className="font-thin hover:text-primary">
+              Become a partner
+            </Link>
+            <Link href="#" className="font-thin hover:text-primary">
+              Partner login
+            </Link>
           </div>
 
-          <div className="space-y-3 flex-1">
+          <div className="space-y-3 flex flex-col">
             <p className="mb-1">Patient Safety </p>
-            <p className="font-thin">Legal documantation</p>
+            <Link href="#" className="font-thin hover:text-primary">
+              Legal documentation
+            </Link>
           </div>
 
-          <div className="space-y-3 flex-1">
+          <div className="space-y-3 flex flex-col">
             <p className="mb-1">Patient</p>
-            <p className="font-thin">Clinics</p>
-            <p className="font-thin">Doctors</p>
-            <p className="font-thin">Medical Consultant</p>
+            <Link href="#" className="font-thin hover:text-primary">
+              Clinics
+            </Link>
+            <Link href="#" className="font-thin hover:text-primary">
+              Doctors
+            </Link>
+            <Link href="#" className="font-thin hover:text-primary">
+              Medical Consultant
+            </Link>
           </div>
 
-          <div className="space-y-3 flex-1">
+          <div className="space-y-3 flex flex-col">
             <p className="mb-1">About us</p>
-            <p className="font-thin">How it works?</p>
-            <p className="font-thin">Our medical team</p>
+            <Link href="#" className="font-thin hover:text-primary">
+              How it works?
+            </Link>
+            <Link href="#" className="font-thin hover:text-primary">
+              Our medical team
+            </Link>
           </div>
         </div>
 
