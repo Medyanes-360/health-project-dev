9eabeaaf3bd0316a7e6ed23d75a3735723cac9b3
justsedaunal <justsedaunal@outlook.com>
2024-11-20 17:37:57 +0300

    data rendered

--- components/clinics/clinicsByTreatmentSearchPage/reusables/Reviews.jsx
+++ components/clinics/clinicsByTreatmentSearchPage/reusables/Reviews.jsx
@@ -1,3 +1,107 @@
+import List from "@/globalElements/doctors&reviewsList";
+import Image from "next/image";
+
 export default function Reviews() {
-  return <div>reviews</div>;
+  return (
+    <div>
+      <ul role="list" class="divide-y divide-gray-100">
+        <li class="flex justify-between gap-x-6 p-2 m-2 bg-[#F9F9F9] items-center rounded-[10px]">
+          <div class="flex min-w-0 gap-x-4">
+            <img
+              class="size-12 flex-none rounded-full bg-gray-50"
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+            <div class="min-w-0 flex-auto">
+              <p class="text-sm/6 font-semibold text-gray-900">
+                Leslie Alexander
+              </p>
+              <p class="mt-1 truncate text-xs/5 text-gray-500">
+                leslie.alexander@example.com
+              </p>
+            </div>
+          </div>
+          <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-start">
+            <p class="text-sm/6 text-gray-900">Co-Founder / CEO</p>
+            <p class="mt-1 text-xs/5 text-gray-500">
+              Last seen <time datetime="2023-01-23T13:23Z">3h ago</time>
+            </p>
+          </div>
+          <div>
+            <Image
+              src={"/assets/clinic/arrow-square-right.svg"}
+              alt="arrow-square-right"
+              width={24}
+              height={24}
+              className=""
+            />
+          </div>
+        </li>
+        <li class="flex justify-between gap-x-6 p-2 m-2 bg-[#F9F9F9] items-center rounded-[10px]">
+          <div class="flex min-w-0 gap-x-4">
+            <img
+              class="size-12 flex-none rounded-full bg-gray-50"
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+            <div class="min-w-0 flex-auto">
+              <p class="text-sm/6 font-semibold text-gray-900">
+                Leslie Alexander
+              </p>
+              <p class="mt-1 truncate text-xs/5 text-gray-500">
+                leslie.alexander@example.com
+              </p>
+            </div>
+          </div>
+          <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-start">
+            <p class="text-sm/6 text-gray-900">Co-Founder / CEO</p>
+            <p class="mt-1 text-xs/5 text-gray-500">
+              Last seen <time datetime="2023-01-23T13:23Z">3h ago</time>
+            </p>
+          </div>
+          <div>
+            <Image
+              src={"/assets/clinic/arrow-square-right.svg"}
+              alt="arrow-square-right"
+              width={24}
+              height={24}
+              className=""
+            />
+          </div>
+        </li>
+        <li class="flex justify-between gap-x-6 p-2 m-2 bg-[#F9F9F9] items-center rounded-[10px]">
+          <div class="flex min-w-0 gap-x-4">
+            <img
+              class="size-12 flex-none rounded-full bg-gray-50"
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+            <div class="min-w-0 flex-auto">
+              <p class="text-sm/6 font-semibold text-gray-900">
+                Leslie Alexander
+              </p>
+              <p class="mt-1 truncate text-xs/5 text-gray-500">
+                leslie.alexander@example.com
+              </p>
+            </div>
+          </div>
+          <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-start">
+            <p class="text-sm/6 text-gray-900">Co-Founder / CEO</p>
+            <p class="mt-1 text-xs/5 text-gray-500">
+              Last seen <time datetime="2023-01-23T13:23Z">3h ago</time>
+            </p>
+          </div>
+          <div>
+            <Image
+              src={"/assets/clinic/arrow-square-right.svg"}
+              alt="arrow-square-right"
+              width={24}
+              height={24}
+              className=""
+            />
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
