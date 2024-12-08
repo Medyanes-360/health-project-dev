1dcf43c69b09e7300796eb02857284f82cbbbab1
ozudev <ozudev@gmail.com>
2024-12-09 00:02:16 +0300

    a

--- containers/blog/blogHomePage/index.jsx
+++ containers/blog/blogHomePage/index.jsx
@@ -5,10 +5,6 @@ export default function BlogHomePageContainer() {
   return (
     <>
       <BreadCrumbComponent />
-      <p className="text-3xl">
-        Bloglar için home page. Burada son yazılar, popüler yazılar ,editörün
-        seçtikleri gibi şeyler olabilir.
-      </p>
     </>
   );
 }
