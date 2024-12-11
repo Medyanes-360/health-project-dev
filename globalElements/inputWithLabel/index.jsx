720dc162dc440e6078ee602e2274bc1d3513fb7f
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-12-11 18:21:48 +0300

    Dashboard - Created InputWithLabel component

---
+++ globalElements/inputWithLabel/index.jsx
@@ -0,0 +1,31 @@
+export const InputWithLabel = (props) => {
+  ///////// Formik ile uyumlu proplar///////////
+  //  component={InputWithLabel}
+  //  name="name"
+  //  label="Name"
+  //  error={errors.name}
+  //  isTouched={touched.name}
+  //  onBlur={handleBlur}
+  //  onChange={handleChange}
+
+  return (
+    <div className="flex flex-col h-full w-full relative">
+      <label
+        className={`bg-white ${
+          props.isTouched && props.error ? "text-red-600" : "text-[#919EAB]"
+        } text-xs font-light absolute left-6 -top-2 `}
+      >
+        {props.label}
+      </label>
+      <input
+        {...props}
+        name={props.field.name}
+        className={`flex flex-col w-full rounded-lg min-h-[40px] border ${
+          props.isTouched && props.error
+            ? "border-red-600 border"
+            : "border-[#919EAB52]"
+        }  px-4`}
+      />
+    </div>
+  );
+};
