a504e1dcff484ad0222a710cfc1ae74140b776dc
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-12-14 18:30:07 +0300

    clinicDashboard

--- app/(dashboardLayout)/dashboard/clinic/page.jsx
+++ app/(dashboardLayout)/dashboard/clinic/page.jsx
@@ -1,13 +1,5 @@
-import Sidebar from "@/components/clinicDashboard/layout/Sidebar";
-import Topbar from "@/components/clinicDashboard/layout/Topbar";
 import ClinicDashboardPageContainer from "@/containers/clinicDashboard";
 
-export default function ClinicDashboardPage() {
-  return (
-    <div className="flex ">
-      <div className="flex-1 flex flex-col">
-        <ClinicDashboardPageContainer />
-      </div>
-    </div>
-  );
+export default function ClinicAdminPage({ children }) {
+  return <ClinicDashboardPageContainer />;
 }
--- containers/clinicDashboard/index.jsx
+++ containers/clinicDashboard/index.jsx
@@ -1,3 +1,27 @@
+"use client";
+
+import Topbar from "@/components/clinicDashboard/layout/Topbar";
+import { useState } from "react";
+
 export default function ClinicDashboardPageContainer() {
-  return <div className="flex-1 p-6 overflow-y-auto">Clinic Dashboard</div>;
+  const [preference, setPreference] = useState("clinic-information");
+
+  return (
+    <div className="flex bg-[#71CCB60F] flex-1 overflow-y-auto">
+      {/* Dashboard Sidebar component */}
+      <div className={`h-screen transition-all duration-300 relative`}>
+        <div
+          className={`bg-white shadow-md px-2 py-2 sm:px-4 sm:py-3 rounded-3xl h-full flex flex-col gap-2
+       `}
+        >
+          Clinic Information <br /> Clinic description
+        </div>
+      </div>
+      <div className="flex-1 flex flex-col">
+        <Topbar />
+        {/* Içerik dinamik olarak sidebarda seçilen preference'a göre değişecek */}
+        <div className="flex-1 p-6 overflow-y-auto">Clinic Dashboard</div>
+      </div>
+    </div>
+  );
 }
