9a8993d28c2b3050bd0fac8f6dea131014153fb6
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-12-24 15:49:49 +0300

    Style Fix

--- globalElements/richTextEditor/common/Button.jsx
+++ globalElements/richTextEditor/common/Button.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const Button =(props)=>{
-    const {children,format,active, ...rest} = props
-    return(
-        <button  className={active?'btnActive':''} title={format}  {...rest} style={{width:'30px',height:'20px',margin:'0 2px'}}>
-            {children}
-        </button>
-    )
-}
+const Button = (props) => {
+  const { children, format, active, ...rest } = props;
+  return (
+    <button
+      className={active ? "btnActive" : "bg-white opacity-50 border-none"}
+      title={format}
+      {...rest}
+      style={{ width: "30px", height: "20px", margin: "0 2px" }}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
